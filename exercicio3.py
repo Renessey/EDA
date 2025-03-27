629fd1741e1a55be88bfe3eba6fa6ac3744e9e93
Renessey <renanneteller20@gmail.com>
2025-03-27 16:49:13 -0300

    Base de função

--- exercicio3.py
+++ exercicio3.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 
 def f(x):
     return x**3 - x - 2
+    
+def bisection(a, b, tol):
+    while (b - a) / 2 > tol:
+        m = (a + b) / 2
+        if f(m) == 0:
+            return m
+        elif f(a) * f(m) < 0:
+            b = m
+        else:
+            a = m
+    return (a + b) / 2
+    
